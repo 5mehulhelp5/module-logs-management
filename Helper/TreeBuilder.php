638cd1e21cf3ba5c49b16d3e72f9effd3c898da9
Jakub Palys <kuba@rossoneri@gmail.com>
2024-10-02 22:59:45 +0200

    refactor code

--- Helper/TreeBuilder.php
+++ Helper/TreeBuilder.php
@@ -6,11 +6,11 @@ namespace NetBytes\LogsExplorer\Helper;
 
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Filesystem\Io\File;
+use NetBytes\LogsExplorer\Service\ContentReaderInterface;
 
 class TreeBuilder
 {
     public const string ROOT_ID = '#';
-    protected const string LOG_EXT = 'log';
     protected const int FILE_MODE = 0;
     protected const int DIR_MODE = 1;
 
@@ -44,18 +44,44 @@ class TreeBuilder
     public function buildTree(array $items, string $parentId, string $path = ''): array
     {
         foreach ($items as $item) {
-            $relativePath = $path ? $path . '/' . $item['text'] : $item['text'];
-            if (isset($item['filetype']) && $item['filetype'] === self::LOG_EXT) {
-                $this->addToTree($item['text'], $parentId, self::FILE_MODE, $relativePath);
-            } elseif (isset($item['id']) && is_dir($item['id'])) {
-                $this->addToTree($item['text'], $parentId, self::DIR_MODE, $relativePath);
-                $this->fileIo->cd($item['id']);
-                $subItems = $this->fileIo->ls(3);
-                $this->buildTree($subItems, (string)($this->counter - 1), $relativePath);
+            $relativePath = $this->generateRelativePath($item, $path);
+
+            if ($this->isFile($item)) {
+                $this->processFileItem($item, $parentId, $relativePath);
+            } elseif ($this->isDirectory($item)) {
+                $this->processDirectoryItem($item, $parentId, $relativePath);
             }
         }
 
         return $this->tree;
+    }
+
+    /**
+     * @param array $item
+     * @param string $parentId
+     * @param string $relativePath
+     *
+     * @return void
+     */
+    protected function processFileItem(array $item, string $parentId, string $relativePath): void
+    {
+        $this->addToTree($item['text'], $parentId, self::FILE_MODE, $relativePath);
+    }
+
+    /**
+     * @param array $item
+     * @param string $parentId
+     * @param string $relativePath
+     *
+     * @return void
+     * @throws LocalizedException
+     */
+    protected function processDirectoryItem(array $item, string $parentId, string $relativePath): void
+    {
+        $this->addToTree($item['text'], $parentId, self::DIR_MODE, $relativePath);
+        $this->fileIo->cd($item['id']);
+        $subItems = $this->fileIo->ls(3);
+        $this->buildTree($subItems, (string)($this->counter - 1), $relativePath);
     }
 
     /**
@@ -68,12 +94,7 @@ class TreeBuilder
      *
      * @return void
      */
-    protected function addToTree(
-        string $text,
-        string $parentId,
-        int $iconMode,
-        string $relativePath
-    ): void {
+    protected function addToTree(string $text, string $parentId, int $iconMode, string $relativePath): void {
         $node = [
             'id'     => $this->counter,
             'parent' => $parentId,
@@ -85,5 +106,36 @@ class TreeBuilder
 
         $this->tree[] = $node;
         $this->counter++;
+    }
+
+    /**
+     * @param array $item
+     * @param string $path
+     *
+     * @return string
+     */
+    private function generateRelativePath(array $item, string $path): string
+    {
+        return $path ? $path . '/' . $item['text'] : $item['text'];
+    }
+
+    /**
+     * @param array $item
+     *
+     * @return bool
+     */
+    private function isFile(array $item): bool
+    {
+        return isset($item['filetype']) && $item['filetype'] === ContentReaderInterface::FILE_EXTENSION;
+    }
+
+    /**
+     * @param array $item
+     *
+     * @return bool
+     */
+    private function isDirectory(array $item): bool
+    {
+        return isset($item['id']) && is_dir($item['id']);
     }
 }
